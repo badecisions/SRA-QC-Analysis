98b4a6ed97c440aee026c85ff9c979632d37f484
Arthur Batista <50253789+badecisions@users.noreply.github.com>
2025-10-22 17:55:03 -0300

    Feat: Add .gitignore to exclude data directories, add shellcheck package to mamba env and add bash pipeline to download, analyse, filter, trim and report changes in SRA data.

---
+++ run_qc_pipeline.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Verificação de argumentos e erros
+set -eu
+
+# Coletando o ID e construindo nomes
+ARQ_SRA=$1
+FILE_RAW="${ARQ_SRA}.fastq.gz"
+FILE_TRIMMED="${ARQ_SRA}_trimmed.fq.gz"
+
+# Organizando os caminhos
+DIR_RAW="1-raw_data"
+DIR_QC="2-qc_reports"
+DIR_TRIM="3-trimmed_data"
+
+# Organizando os arquivos de input e output
+echo "Criando os diretórios"
+mkdir -p $DIR_RAW $DIR_QC $DIR_TRIM
+
+# Download do SRA
+echo "Fazendo o Download do arquivo $FILE_RAW"
+fastq-dump --gzip "$ARQ_SRA" -O $DIR_RAW
+
+# Raw data FastQC
+echo "Gerando o relatório QC do arquivo RAW"
+fastqc "$DIR_RAW/$FILE_RAW" -O $DIR_QC
+
+# Trimando os dados
+echo "Limpando os reads de baixa qualidade e removendo adaptadores"
+trim_galore "$DIR_RAW/$FILE_RAW" -o $DIR_TRIM
+
+# Trimmed data FastQC
+echo "Gerando relatório do $FILE_TRIMMED"
+fastqc "$DIR_TRIM/$FILE_TRIMMED" -o $DIR_QC
+
+# Unindo os relatórios
+echo "Unindo os dois relatórios"
+multiqc $DIR_QC -o $DIR_QC
+
+echo "Pipeline finalizado! O relatório está em ${DIR_QC}/multiqc_report.html"
