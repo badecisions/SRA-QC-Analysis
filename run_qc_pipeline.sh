e3eb4e69a7319e5bfe95e62736e8a42a00b84641
Arthur Batista <50253789+badecisions@users.noreply.github.com>
2025-10-26 12:14:52 -0300

    refactor: improving clarity and consistency of pipeline status messages

--- run_qc_pipeline.sh
+++ run_qc_pipeline.sh
@@ -12,37 +12,37 @@ DIR_QC="2-qc_reports"
 DIR_TRIM="3-trimmed_data"
 
 # Organizando os arquivos de input e output
-echo "[1/6] Criando os diretórios"
+echo "[1/6] Creating directories..."
 mkdir -p $DIR_RAW $DIR_QC $DIR_TRIM
 
 # Loop para download dos SRAs
-echo -e "\n[2/6] Download dos SRAs"
+echo -e "\n[2/6] Downloading SRA files..."
 for ID in "${ARQS_SRA[@]}"
 do
-    echo "Baixando $ID"
+    echo "Downloading $ID"
     fastq-dump --gzip "$ID" -O $DIR_RAW
 done
 
 # Raw data FastQC
-echo -e "\n[3/6] Gerando o relatório QC dos arquivos RAW"
+echo -e "\n[3/6] Running FastQC on raw files..."
 fastqc ${DIR_RAW}/*.fastq.gz -O $DIR_QC
 
 # Trimando os dados
-echo -e "\n[4/6] Limpando os reads de baixa qualidade e removendo adaptadores"
+echo -e "\n[4/6] Trimming low-quality bases and adapters..."
 for ID in "${ARQS_SRA[@]}"
 do
-    echo -e "\n Triando o arquivo $ID"
+    echo -e "\n$ID"
     trim_galore "${DIR_RAW}/${ID}.fastq.gz" -o $DIR_TRIM
 done
 
 # Trimmed data FastQC
-echo -e "\n[5/6] Gerando relatório dos arquivos triados"
+echo -e "\n[5/6] Running FastQC on trimmed files..."
 fastqc ${DIR_TRIM}/*fq.gz -o $DIR_QC
 
 # Unindo os relatórios
-echo -e "\n[6/6] Unindo os relatórios"
+echo -e "\n[6/6] Aggregating QC reports with MultiQC..."
 multiqc -s $DIR_QC -o $DIR_QC
 
 echo ""
-echo "Pipeline finalizado!" 
-echo "O relatório está em ${DIR_QC}/multiqc_report.html"
+echo "Pipeline finished successfully!" 
+echo "Final report available at: ${DIR_QC}/multiqc_report.html"
