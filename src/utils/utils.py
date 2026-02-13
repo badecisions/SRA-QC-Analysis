a40b8b29c8ff6f12a9ee9f63f9fb1c4f8066ebe1
Arthur Batista <arthurbatistant@gmail.com>
2026-02-13 16:00:11 -0300

    refactor: muda a lógica de criação de diretórios para o módulo utils

---
+++ src/utils/utils.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+import subprocess
+
+# function to verify the existence of the directories
+def directories_verify():
+    """Verifica a presença de todos os diretórios necessários, e caso não existam cria eles."""
+    print("Verificando a integridade das pastas...")
+    results_base = Path("results")
+    results_raw = results_base / "01_fastqc_raw"
+    results_report = results_base / "02_fastp_report"
+    results_clean = results_base / "03_fastqc_clean"
+    results_multi = results_base / "04_multiqc"
+    
+    logs = Path("logs")
+
+    data_base = Path("data")
+    data_raw = data_base / "raw"
+    data_processed = data_base / "processed"
+    data_refs = data_base / "refs"
+
+    directories = [results_base, results_raw, results_report, 
+                   results_clean, results_multi, logs, 
+                   data_base, data_raw, data_processed, data_refs]
+
+    for dir in directories:
+        dir.mkdir(parents=True)
+
+# function to verify conda
