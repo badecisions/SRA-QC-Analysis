fdf6a110fcdf1d0114213c3abdc9e9ab7e5fc9c9
Arthur Batista <arthurbatistant@gmail.com>
2026-02-16 16:40:43 -0300

    feat: adiciona novos argumentos, atualiza a descrição e inclui o módulo de download dos SRAs

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 
-from utils.directories import directories_verify
-from utils.conda_verify import conda_verify
-from utils.verify_id import verify_valid_id
+from utils import create_directories, conda_verify, verify_valid_id
+from modules import sra_downloader
 import sys
 from argparse import ArgumentParser
 
 # configurando o parser
-parser = ArgumentParser(description="Pipeline SRA -> QC -> Trim")
+parser = ArgumentParser(description="Pipeline para o Download, Verificação e Limpeza de arquivos SRA.")
 
 parser.add_argument("--sra", required=True,
-                    help="ID de acesso do SRA", type=str, nargs="+")
+                    help="IDs de acesso do SRA", type=str, nargs="+")
 parser.add_argument("--outdir", default="results",
-                    help="Pasta onde os resultados serão salvos (Padrão: results/)")
+                    help="Diretório onde os resultados serão salvos (Padrão: results/)")
+parser.add_argument("--data", default="data",
+                    help="Diretório onde os dados, brutos e processados, serão salvos (Padrão: data/)")
 
 args = parser.parse_args()
 
 # verificando diretórios e instalação do conda
-directories_verify(args.outdir)
+create_directories(args.outdir, args.data)
 
 conda_verify()
 
@@ -24,3 +25,5 @@ conda_verify()
 sra_user_ids = args.sra
 
 valid_ids = verify_valid_id(sra_ids=sra_user_ids)
+
+down_sra = sra_downloader(sra_ids=valid_ids, download_path=args.data)
