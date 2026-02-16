f49342131c05a221e3ebfc2dc5ae1c6728c8c288
Arthur Batista <arthurbatistant@gmail.com>
2026-02-16 15:24:34 -0300

    style: torna mais intuitivos os outputs da função

--- utils/conda_verify.py
+++ utils/conda_verify.py
@@ -3,6 +3,7 @@ import sys, subprocess
 # function to verify conda
 def conda_verify():
     """Verifica a instação do conda e também a presença das ferramentas da pipeline no ambiente"""
+    print("\nVERIFICANDO O CONDA")
 
     command_conda = ["conda", "--version"]
     command_tools = ["fastqc", "--version", "&&", 
@@ -13,14 +14,14 @@ def conda_verify():
     resp_conda = subprocess.run(command_conda, capture_output=True, text=True)
 
     if resp_conda.returncode == 0:
-        print("Instalação do conda encontrada!")
+        print("CONDA: Instalação encontrada!")
     else:
         sys.exit("ERRO: Conda não instalado")
 
     try:     
         resp_tools = subprocess.run(command_tools, capture_output=True, text=True)
         if resp_tools.returncode == 0:
-            print("Ferramentas encontradas!")
+            print("CONDA: Ambiente ativo e Ferramentas encontradas!")
     except FileNotFoundError:
         print("\nERRO: Nenhuma ferramenta encontrada.")
-        sys.exit("Ative o ambiente sra_qc -> conda activate sra_qc")
+        sys.exit("CONDA: Ative o ambiente sra_qc -> conda activate sra_qc")
