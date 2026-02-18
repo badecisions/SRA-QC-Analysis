aa73f8c72185206457d0ebed3f9346ecb83c0b77
Arthur Batista <arthurbatistant@gmail.com>
2026-02-18 13:43:22 -0300

    fix: resolve o IndexError quanto um download falha

--- modules/downloader.py
+++ modules/downloader.py
@@ -1,7 +1,6 @@
 
 from pathlib import Path
 import sys, subprocess
-from utils import verify_valid_id
 
 
 def sra_downloader(sra_ids:list, download_path:str) -> list:
@@ -10,27 +9,29 @@ def sra_downloader(sra_ids:list, download_path:str) -> list:
     
     print("\nBAIXANDO OS ARQUIVOS SRA")
 
-    valid_sra = verify_valid_id(sra_ids=sra_ids)
     download = Path(download_path)
     download = download / "raw"
+    contador = 1
 
-    for i in range(len(valid_sra)):
-        command_download = ["fasterq-dump", valid_sra[i], "-O", download, "-x"]
+    for i in sra_ids:
+        command_download = ["fasterq-dump", i, "-O", download, "-x"]
 
-        print(f"Download {i+1}/{len(valid_sra)}: {valid_sra[i]}")
+        print(f"Download {contador}/{len(sra_ids)}: {i}")
 
-        log_path = Path("logs") / f"{valid_sra[i]}_fasterq-dump.log"
+        log_path = Path("logs") / f"{i}_fasterq-dump.log"
 
         with open(log_path, "w") as log_file:
             result = subprocess.run(command_download, stdout=log_file, stderr=log_file, text=True)
 
         if result.returncode != 0:
-            print(f"Não foi possível baixar o SRA ID: {valid_sra[i]}")
-            valid_sra.remove(valid_sra[i])
+            print(f"Não foi possível baixar o SRA ID: {i}")
+            sra_ids.remove(i)
 
-    if valid_sra == []:
+            contador += 1
+
+    if sra_ids == []:
         sys.exit("ERRO: Nenhum ID pôde ser baixado.")
     else:
         print("\nArquivos baixados com sucesso:")
-        print('\n'.join(i for i in valid_sra))
-        return valid_sra
+        print('\n'.join(i for i in sra_ids))
+        return sra_ids
