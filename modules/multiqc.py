6a73408fcbf6485e2e6613e7daff8a83649d1930
Arthur Batista <arthurbatistant@gmail.com>
2026-02-26 16:54:59 -0300

    fix: corrige a adição do comando do multiqc no .log

--- modules/multiqc.py
+++ modules/multiqc.py
@@ -8,11 +8,11 @@ def run_multiqc(result_path:str):
 
     output_dir = Path(result_path) / "04_multiqc"
     
-    commmand_multiqc = ["multiqc", result_path, "-o", output_dir]
+    command_multiqc = ["multiqc", result_path, "-o", output_dir]
     
-    logger.info(f"MultiQC: Commando {' '.join(i for i in commmand_multiqc)}")
+    logger.info(f"MultiQC: Commando {' '.join(map(str, command_multiqc))}")
 
-    multiqc = subprocess.run(commmand_multiqc, capture_output=True, text=True)
+    multiqc = subprocess.run(command_multiqc, capture_output=True, text=True)
 
     if multiqc.returncode != 0:
         print(f"ERRO no MULTIQC")
