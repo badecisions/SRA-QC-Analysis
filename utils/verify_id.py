476b8cf04008c9dc02b9240e88d724d9bac3ec9d
Arthur Batista <arthurbatistant@gmail.com>
2026-03-18 12:21:09 -0300

    refactor: torna as comparações mais idiomáticas

--- utils/verify_id.py
+++ utils/verify_id.py
@@ -10,15 +10,16 @@ def verify_valid_id(sra_ids:list) -> list:
     valid_sra_ids = []
 
     for i in sra_ids:
-        if re.match(padrao, i) == None:
+        if re.match(padrao, i) is None:
             print(f"O ID {i} é inválido")
             logger.error(f"ID inválido: {i}")
         else:
             valid_sra_ids.append(i)
 
-    if len(valid_sra_ids) == 0:
+    if not valid_sra_ids:
         print("Nenhum dos IDs digitados são válidos")
         logger.error("ERRO: Nenhum ID válido.")
         sys.exit("ERRO: Nenhum ID válido.")
     else:
+        logger.info(f"IDs válidos: {' '.join(valid_sra_ids)}")
         return valid_sra_ids
